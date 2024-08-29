611f7efd8be671a84108533a46c7fcdccc957890
Matt DiRisio <dirisiom@msu.edu>
2024-08-29 19:20:08 -0400

    created board, made it create the balloons

---
+++ src/elements/board.py
@@ -0,0 +1,29 @@
+import pygame as pg
+import random
+from src.elements.balloon import Balloon, Color
+
+# define constants for num of balloons in a row (15) and num in a column (10)
+ROW_LEN = 15  # num of columns
+COL_LEN = 10  # num of rows
+
+# "game board" that contains all the balloons, as well as powerups (Eventually)
+def create_balloon(pos):
+    return Balloon(random.choice(list(Color)), pos=pos)
+
+class Board:
+    def __init__(self):
+        self.table = []
+        position = (120, 50)
+        for i in range(COL_LEN):
+            self.table.append([])
+            for j in range(ROW_LEN):
+                self.table[i].append(create_balloon(position))
+                position = (position[0] + 50, position[1])
+            position = (120, position[1] + 50)
+        print()
+
+    def draw(self, screen):
+        for row in self.table:
+            for balloon in row:
+                balloon.draw(screen)
+
