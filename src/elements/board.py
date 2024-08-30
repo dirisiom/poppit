7b61f198f9d563ca7b1353f6a0dff9273ba438a3
Matt DiRisio <dirisiom@msu.edu>
2024-08-29 23:54:32 -0400

    removed unnecessary

--- src/elements/board.py
+++ src/elements/board.py
@@ -35,7 +35,6 @@ class Board:
     def draw(self, screen):
         gift_img = pg.image.load("src/assets/gift-box.svg")
         gift_img = pg.transform.scale(gift_img, (20, 20))
-        gift_rect = gift_img.get_rect()
 
         for row in range(COL_LEN):
             for col in range(ROW_LEN):
